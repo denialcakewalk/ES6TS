96ec7717dd39770edae9a50231b8f12d1aa9e959
Denial Tandel <denialtandel@cakewalk.in>
2017-05-26 14:54:05 +0530

    Project template

---
+++ ES6TSApp/ES6TSApp/webpack.config.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+    entry: {
+        //a: './Controller/Base1.js',
+        //b: './Controller/Register.js'
+    },
+    output: {
+        //path: path.join(__dirname, "dist"),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    }
+};
